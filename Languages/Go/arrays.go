88cce6dd36c307f2504ae8552e6ab042bdb63e3a
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-05 23:18:24 +0100

    GO maps

--- Languages/Go/arrays.go
+++ Languages/Go/arrays.go
@@ -31,4 +31,36 @@ func main() {
 	fmt.Printf("classic languagues: %v\n", classics) // classic languagues: [C Lisp C++]
 	fmt.Printf("modern languages: %v\n", modern)     // modern languages: [Java Python JavaScript Ruby]
 	fmt.Printf("new languages: %v\n", new)           // new languages: [Go Rust]
+
+	////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////
+
+	allLangs := languages[:]                      // copy of the array
+	fmt.Println(reflect.TypeOf(allLangs).Kind())   // slice
+
+	/* Create a slice containing web frameworks */
+	frameworks := []string{
+		"React", "Vue", "Angular", "Svelte",
+		"Laravel", "Django", "Flask", "Fiber",
+	}
+
+	jsFrameworks := frameworks[0:4:4]          // length 4 capacity 4
+	frameworks = append(frameworks, "Meteor")  // not possible with arrays
+
+	fmt.Printf("all frameworks: %v\n", frameworks)
+	fmt.Printf("js frameworks: %v\n", jsFrameworks)
+
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	////////// MAPS///////////
+	
+	firstReleases := map[string]int{
+		"C": 1972, "C++": 1985, "Java": 1996,
+		"Python": 1991, "JavaScript": 1996, "Go": 2012,
+	}
+
+	/* Loop through each entry and output the name and release year */
+	for k, v := range firstReleases {
+		fmt.Printf("%s was first released in %d\n", k, v)
+	}
 }
